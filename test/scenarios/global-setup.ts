247a308c020316ff5b05bca85ab9fa1fc2f2bc70
Timshel <Timshel@users.noreply.github.com>
2024-02-03 23:10:06 +0100

    Improve playwright tests

---
+++ test/scenarios/global-setup.ts
@@ -0,0 +1,90 @@
+import { firefox, type FullConfig } from '@playwright/test';
+import { exec, execSync } from 'node:child_process';
+import fs from 'fs';
+import yaml from 'js-yaml';
+
+const utils = require('./global-utils');
+
+utils.loadEnv();
+var kcPath = process.env.KC_SETUP_PATH;
+
+function readCurrentVersion(){
+    try {
+        const vw_version_file = fs.readFileSync('data/web-vault/vw-version.json', {
+            encoding: 'utf8',
+            flag: 'r'
+        });
+
+        return JSON.parse(vw_version_file)["version"];
+    } catch(err) {
+        console.log(`Failed to read frontend current version: ${err}`);
+    }
+}
+
+function readDockerVersion(){
+    try {
+        const docker_settings = fs.readFileSync('../../docker/DockerSettings.yaml', {
+            encoding: 'utf8',
+            flag: 'r'
+        });
+
+        const settings = yaml.load(docker_settings);
+        return settings["vault_version"];
+    } catch(err) {
+        console.log(`Failed to read docker frontend current version: ${err}`);
+    }
+}
+
+function retrieveFrontend(){
+    const vw_version = readCurrentVersion();
+    const vv = readDockerVersion()
+
+    if( !vv ){
+        console.log("Empty docker frontend version");
+        process.exit(1);
+    }
+
+    try {
+        if( vv != `v${vw_version}`) {
+            fs.rmSync("./data/web-vault", { recursive: true, force: true });
+
+            execSync(`cd data && wget -c https://github.com/dani-garcia/bw_web_builds/releases/download/${vv}/bw_web_${vv}.tar.gz -O - | tar xz`, { stdio: "inherit" });
+
+            // Make the SSO button visible
+            execSync(`bash -c "sed -i 's#a.routerlink=./sso..,##' data/web-vault/app/main.*.css"`, { stdio: "inherit" });
+
+            console.log(`Retrieved bw_web_builds-${vv}`);
+        } else {
+            console.log(`Using existing bw_web_builds-${vv}`);
+        }
+    } catch(err) {
+        console.log(`Failed to retrieve frontend: ${err}`);
+        process.exit(1);
+    }
+}
+
+function buildServer(){
+    if( !fs.existsSync('data/vaultwarden') ){
+        console.log("Rebuilding server");
+        execSync(`cd ../.. && cargo build --features sqlite --release`, { stdio: "inherit" });
+        execSync(`cp ../../target/release/vaultwarden data/vaultwarden`, { stdio: "inherit" });
+    } else {
+        console.log("Using existing server");
+    }
+}
+
+async function globalSetup(config: FullConfig) {
+    execSync("mkdir -p data/logs");
+
+    buildServer();
+    retrieveFrontend();
+
+    exec(`ENV=test KC_SETUP_PATH=${kcPath} docker-compose -f ${kcPath}/docker-compose.yml  --project-directory . up >> data/logs/keycloak.log 2>&1`);
+
+    var browser = await firefox.launch();
+    await utils.waitFor(process.env.SSO_AUTHORITY, browser);
+    await browser.close();
+    console.log(`Keycloak running on: ${process.env.SSO_AUTHORITY}`);
+}
+
+export default globalSetup;
--- test/scenarios/playwright.config.ts
+++ test/scenarios/playwright.config.ts
@@ -1,48 +1,49 @@
 import { defineConfig, devices } from '@playwright/test';
+import dotenv from 'dotenv';
+import dotenvExpand from 'dotenv-expand';
+import { exec } from 'node:child_process';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
+const utils = require('./global-utils');
+
+utils.loadEnv();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './.',
-  /* Run tests in files in parallel */
-  fullyParallel: false,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  workers: 1,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
-  timeout: 10 * 1000,
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://127.0.0.1:8000',
+    testDir: 'tests',
+    /* Run tests in files in parallel */
+    fullyParallel: false,
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-  },
+    /* Fail the build on CI if you accidentally left test.only in the source code. */
+    forbidOnly: !!process.env.CI,
 
-  /* Configure projects for major browsers */
-  projects: [
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+    /* Retry on CI only */
+    retries: process.env.CI ? 2 : 0,
+    workers: 1,
+
+    /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+    reporter: 'html',
+    timeout: 10 * 1000,
+    expect: { timeout: 10 * 1000 },
+
+    /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+    use: {
+        /* Base URL to use in actions like `await page.goto('/')`. */
+        baseURL: process.env.DOMAIN,
+
+        /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+        trace: 'on-first-retry',
     },
-  ],
 
-  /* Run your local dev server before starting the tests */
-  webServer: {
-      command: 'cd ../oidc && docker-compose --profile VaultWarden up --force-recreate -V',
-      url: 'http://127.0.0.1:8080/realms/test',
-      reuseExistingServer: !process.env.CI,
-      timeout: 300 * 1000
-  }
+    /* Configure projects for major browsers */
+    projects: [
+        {
+            name: 'firefox',
+            use: { ...devices['Desktop Firefox'] },
+        },
+    ],
+
+    globalSetup: require.resolve('./global-setup'),
+    globalTeardown: require.resolve('./global-teardown'),
 });
